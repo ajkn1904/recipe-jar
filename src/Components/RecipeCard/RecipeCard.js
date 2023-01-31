0cfbb9ede612bbc8d17e42b4402f78f3cddcac62
ajkn1904 <ajknishat@gmail.com>
2023-01-31 23:53:06 +0600

    recipes loaded

---
+++ src/Components/RecipeCard/RecipeCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const RecipeCard = ({ recipe }) => {
+
+    const { _id, name, image, cooking_description, userName } = recipe;
+
+
+    return (
+        <div className="bg-base-100 border border-gray-400 shadow-xl relative">
+
+            <figure><img src={image} alt={name} className='w-full mx-auto h-72' /></figure>
+
+            <div className="badge bg-gradient-to-r from-[#3c597a] to-[#41ad35] p-3 font-semibold relative bottom-64 left-60">
+                <FaUser className='mr-2' /> {userName}</div>
+            <div className="card-body">
+
+                <h2 className="card-title mb-6">{name}</h2>
+
+                <p>{cooking_description.slice(0, 70) + '...'}</p>
+
+                <div className="card-actions">
+                    <button className="btn bg-[#fa894d] text-white hover:bg-[#d66223] my-8 mx-auto"><Link to={`/recipe/${_id}`}>View Details</Link></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecipeCard;
--- src/Components/Recipes/Recipes.js
+++ src/Components/Recipes/Recipes.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../Context/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import RecipeCard from '../RecipeCard/RecipeCard';
 
 const Recipes = () => {
+
+    const { loading } = useContext(AuthContext)
+
+
+    const { data = [], isLoading, refetch } =
+        useQuery({
+            queryKey: ['recipes'],
+
+            queryFn: async () => {
+                const res = await fetch('http://localhost:5000/recipes');
+                const data = await res.json();
+                return data;
+            }
+        })
+
+        console.log(data)
+
+
+    if (loading || isLoading) {
+        return <button className="btn btn-ghost text-red-700 loading"></button>
+    }
+
+
     return (
         <div>
-            this is recipes
+        <h1 className='text-center text-5xl font-bold mt-14 mb-8'>All Recipes</h1>
+            
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 mx-12 sm:mx-12 md:mx-14 lg:mx-20 my-20'>
+                {
+                    data.map(recipe => <RecipeCard recipe={recipe} key={recipe._id}></RecipeCard>)
+                }
+            </div>
         </div>
     );
 };
