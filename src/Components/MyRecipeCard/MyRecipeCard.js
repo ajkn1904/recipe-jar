9f5874b2f8bca188bda37693e354b43cf096f686
ajkn1904 <ajknishat@gmail.com>
2023-02-01 20:53:51 +0600

    my recipes added

---
+++ src/Components/MyRecipeCard/MyRecipeCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiEdit } from 'react-icons/bi'
+import { MdDeleteOutline } from 'react-icons/md'
+
+const MyRecipeCard = ({ recipe }) => {
+
+    const { _id, name, image, ingredients, cooking_description, userName, userEmail } = recipe;
+
+    return (
+        <div className="card w-[90%] sm:w-[90%] md:w-[95%] lg:w-[80%] mx-auto bg-base-100 md:card-side lg:card-side shadow-xl" key={_id}>
+            <figure><img src={image} alt="book" className='w-full md:w-72 h-[370px]' /></figure>
+            <div className="card-body md:w-5/12 lg:w-8/12">
+                <h2 className="card-title">{name}</h2>
+
+                <div><strong>Ingredients: </strong>
+                    {
+                        ingredients.map(data => <span key={data}>{data}, </span>)
+                    }
+
+                    <div className='mt-6'>
+                        <strong>Description: </strong>
+                        <span>{cooking_description.slice(0, 350) + '...'}</span>
+                    </div>
+                </div>
+
+                <div className="card-actions flex justify-between items-center">
+                    <button className="btn bg-[#fa894d] btn-sm text-white hover:bg-[#d66223] my-4"><Link to={`/recipes/${_id}`}>View Details</Link></button>
+
+                    <div className='flex items-center gap-5'>
+                        <BiEdit className='h-10 w-10 bg-green-200 rounded-full p-2 hover:text-white hover:bg-green-800' />
+                        <MdDeleteOutline className='h-10 w-10 bg-red-200 rounded-full p-2 hover:text-white hover:bg-red-700' />
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default MyRecipeCard;
--- src/Components/MyRecipes/MyRecipes.js
+++ src/Components/MyRecipes/MyRecipes.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../Context/AuthProvider';
+import MyRecipeCard from '../MyRecipeCard/MyRecipeCard';
 
 const MyRecipes = () => {
+
+    const { user } = useContext(AuthContext);
+
+    const { data = [], isLoading, refetch } = useQuery({
+        queryKey: ['recipes'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/users/recipes?email=${user?.email}`);
+
+            const data = res.json()
+            return data;
+
+        }
+    });
+
+
+    console.log(data)
+
+    if (isLoading) {
+        return <button className="btn btn-ghost text-red-700 loading"></button>
+    }
+
     return (
-        <div>
-           this is my recipes 
+        <div className='w-[80%] mx-auto min-h-screen my-20'>
+        <h1 className='text-4xl text-center font-semibold mb-10'>My Recipes</h1>
+
+{ user?.uid && 
+        <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-12 sm:gap-12 md:gp-9 lg:gap-9 my-24'>
+            {data &&
+                data?.map(recipe => <MyRecipeCard recipe={recipe} key={recipe._id} refetch={refetch} />)
+            }
         </div>
+}
+
+    </div>
     );
 };
 
